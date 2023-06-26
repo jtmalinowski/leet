8cdb461dffe78b10c6d7f86b60d139615a62ee08
Jakub Malinowski <jakubmal+github@gmail.com>
2023-06-26 23:33:16 +0200

    fix: formatting 2462

--- 2462-total-cost-workers/app.go
+++ 2462-total-cost-workers/app.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-  "container/heap"
-  "fmt"
+	"container/heap"
+	"fmt"
 )
 
 // An Item is something we manage in a cost queue.
 type Item struct {
-	cost int
-  originalIndex int
-	_index int
+	cost          int
+	originalIndex int
+	_index        int
 }
 
 type PriorityQueue []*Item
@@ -17,9 +17,9 @@ type PriorityQueue []*Item
 func (pq PriorityQueue) Len() int { return len(pq) }
 
 func (pq PriorityQueue) Less(i, j int) bool {
-  if (pq[i].cost == pq[j].cost) {
-    return pq[i].originalIndex < pq[j].originalIndex  
-  }
+	if pq[i].cost == pq[j].cost {
+		return pq[i].originalIndex < pq[j].originalIndex
+	}
 	return pq[i].cost < pq[j].cost
 }
 
@@ -40,73 +40,85 @@ func (pq *PriorityQueue) Pop() any {
 	old := *pq
 	n := len(old)
 	item := old[n-1]
-	old[n-1] = nil  // avoid memory leak
+	old[n-1] = nil   // avoid memory leak
 	item._index = -1 // for safety
 	*pq = old[0 : n-1]
 	return item
 }
 
 func totalCost(costs []int, k int, candidates int) int64 {
-  pq := make(PriorityQueue, 0)
-  heap.Init(&pq)
+	pq := make(PriorityQueue, 0)
+	heap.Init(&pq)
 
-  nextUp := 0
-  for ; ; nextUp++ {
-    if nextUp >= candidates { break }
-    if nextUp >= len(costs) { break }
+	nextUp := 0
+	for ; ; nextUp++ {
+		if nextUp >= candidates {
+			break
+		}
+		if nextUp >= len(costs) {
+			break
+		}
 
-    heap.Push(&pq, &Item{
-      cost: costs[nextUp],
-      originalIndex: nextUp,
-    })
-  }
+		heap.Push(&pq, &Item{
+			cost:          costs[nextUp],
+			originalIndex: nextUp,
+		})
+	}
 
-  nextDown := len(costs) - 1
-  for {
-    if (nextDown <= len(costs) - candidates - 1) { break }
-    if (nextDown <= nextUp) { break }
+	nextDown := len(costs) - 1
+	for {
+		if nextDown <= len(costs)-candidates-1 {
+			break
+		}
+		if nextDown <= nextUp {
+			break
+		}
 
-    heap.Push(&pq, &Item{
-      cost: costs[nextDown],
-      originalIndex: nextDown,
-    })
-    nextDown--
-  }
+		heap.Push(&pq, &Item{
+			cost:          costs[nextDown],
+			originalIndex: nextDown,
+		})
+		nextDown--
+	}
 
-  sum := int64(0)
-  taken := 0
+	sum := int64(0)
+	taken := 0
 
-  for {
-    nextItem := heap.Pop(&pq).(*Item)
+	for {
+		nextItem := heap.Pop(&pq).(*Item)
 
-    sum += int64(nextItem.cost)
-    taken++
+		sum += int64(nextItem.cost)
+		taken++
 
-    if (len(pq) == 0 || taken == k) { break }
+		if len(pq) == 0 || taken == k {
+			break
+		}
 
-    if (nextUp > nextDown) { continue }
-    if (nextItem.originalIndex < nextUp) {
-      heap.Push(&pq, &Item{
-        cost: costs[nextUp],
-        originalIndex: nextUp,
-      })
-      nextUp++
-    } else {
-      heap.Push(&pq, &Item{
-        cost: costs[nextDown],
-        originalIndex: nextDown,
-      })
-      nextDown--
-    }
-  }
+		if nextUp > nextDown {
+			continue
+		}
+		if nextItem.originalIndex < nextUp {
+			heap.Push(&pq, &Item{
+				cost:          costs[nextUp],
+				originalIndex: nextUp,
+			})
+			nextUp++
+		} else {
+			heap.Push(&pq, &Item{
+				cost:          costs[nextDown],
+				originalIndex: nextDown,
+			})
+			nextDown--
+		}
+	}
 
-  return sum
+	return sum
 }
 
 func main() {
-  fmt.Println(totalCost([]int{17,12,10,2,7,2,11,20,8}, 3, 4))
-  fmt.Println(totalCost([]int{1,2,4,1}, 3, 3))
-  fmt.Println(totalCost([]int{1,2,3,4}, 5, 5))
-  fmt.Println(totalCost([]int{1,2,3,4}, 1, 2))
-  fmt.Println(totalCost([]int{2,2,2,2,2,2,1,4,5,5,5,5,5,2,2,2,2,2,2,2,2,2,2,2,2,2},7,3))
+	fmt.Println(totalCost([]int{17, 12, 10, 2, 7, 2, 11, 20, 8}, 3, 4))
+	fmt.Println(totalCost([]int{1, 2, 4, 1}, 3, 3))
+	fmt.Println(totalCost([]int{1, 2, 3, 4}, 5, 5))
+	fmt.Println(totalCost([]int{1, 2, 3, 4}, 1, 2))
+	fmt.Println(totalCost([]int{2, 2, 2, 2, 2, 2, 1, 4, 5, 5, 5, 5, 5, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2}, 7, 3))
 }
